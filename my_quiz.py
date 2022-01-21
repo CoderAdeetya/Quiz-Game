c4ca652081afb3d55ade8c2aaec275ca9f4cc4e1
Adeetya Agarwal <98150708+CoderAdeetya@users.noreply.github.com>
2022-01-21 13:57:55 +0530

    Update my_quiz.py

--- my_quiz.py
+++ my_quiz.py
@@ -1,17 +1,20 @@
+#Quiz_game
 print("Welcome to the Quiz!!")
 
+#Asking user whether wanna play or not
 playing = input("Do you want to play? ")
 
-
+#if answer is yes the it will continue else program will end
 if playing.lower() != 'yes':
     quit()
 
 
 print("OK, Lets Begin!!")
 
+#Declaring the score
 score = 0
 
-
+#First Question
 q1 = input("What does CPU stands for? ")
 if q1.lower() == 'central processing unit':
     print("Correct!!")
@@ -20,6 +23,7 @@ if q1.lower() == 'central processing unit':
 else:
     print("Incorrect!")
 
+#Second Question
 q2 = input("What does RAM stands for? ")
 if q2.lower() == 'random access memory':
     print("Correct!!")
@@ -27,6 +31,7 @@ if q2.lower() == 'random access memory':
 else:
     print("Incorrect!")
 
+#Third Question
 q3 = input("What does GPU stands for? ")
 if q3.lower() == 'graphics processing unit':
     print("Correct!!")
@@ -34,7 +39,8 @@ if q3.lower() == 'graphics processing unit':
 
 else:
     print("Incorrect!")
-
+    
+#Fourth Question
 q4 = input("What does PSU stands for? ")
 if q4.lower() == 'power supply':
     print("Correct!!")
@@ -43,4 +49,5 @@ if q4.lower() == 'power supply':
 else:
     print("Incorrect!")
 
+#Displaying the final Score
 print("You Scored", str(score),"answer correct!")
